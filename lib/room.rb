66d092b76b510c78191157dda48b9988169863b4
Regina Casias <reginaannvigil@gmail.com>
2020-12-09 13:18:41 -0700

    initial commit

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,25 @@
-class Rom
+class Room
+  attr_reader :category,
+              :length,
+              :width
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
-    @width = width
+    @length = length.to_i
+    @width = width.to_i
+    @paint = false
   end
 
   def area
     @length * @width
   end
+
+  def is_painted?
+    @paint
+  end
+
+  def paint
+    @paint = true
+  end
+
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -5,20 +5,39 @@ require './lib/room'
 class RoomTest < Minitest::Test
   def test_it_exists
     room = Room.new(:bedroom, 10, '13')
+
     assert_instance_of Room, room
   end
 
   def test_it_has_a_category
-    skip
+
     room = Room.new(:bedroom, 10, '13')
+
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
+
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
+
     assert_equal 130, room1.area
     assert_equal 180, room2.area
   end
+
+  def test_room_is_painted?
+    room1 = Room.new(:bedroom, 10, '13')
+    room2 = Room.new(:living_room, 15, '12')
+
+    assert_equal room1.is_painted?, false
+    assert_equal room2.is_painted?, false
+  end
+
+  def test_paint
+    room1 = Room.new(:bedroom, 10, '13')
+    room1.paint
+
+    assert_equal room1.is_painted?, true
+  end
+
 end
